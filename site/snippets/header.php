d52da14b39e131fce38069326a7de1d1a17df767
Giovanni <gvocale@gmail.com>
2016-01-29 11:47:19 -0500

    commit

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -1,11 +1,26 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-
-  <meta charset="utf-8" />
-  <meta name="viewport" content="width=device-width,initial-scale=1.0">
-
-  <title><?php echo $site->title()->html() ?> | <?php echo $page->title()->html() ?></title>
-
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+    <title>
+        <?php echo $site->title()->html() ?> |
+            <?php echo $page->title()->html() ?>
+    </title>
+    <?php echo css('assets/css/main.css') ?>
 </head>
+
 <body>
+    <nav class="container-fluid">
+        <div class="row">
+            <div class="col-xs-12">
+                <div id="logo" class="logo">
+                    <img src="<?php echo kirby()->urls()->assets() ?>/images/razorfish-logo.svg" alt="" class="razorfish-logo">
+                    <p class="global">Global</p>
+
+                </div>
+                <img src="<?php echo kirby()->urls()->assets() ?>/images/hamburger-icon.svg" alt="" class="hamburger-icon">
+            </div>
+        </div>
+    </nav>
---
+++ site/snippets/text1.php
@@ -0,0 +1,7 @@
+<section class="container-fluid intro-text">
+    <div class="row text-container">
+        <div class="col-xs-10 col-xs-offset-1 col-md-6 col-md-offset-1">
+            <h1><?php echo $section->text()->kirbytext() ?></h1>
+        </div>
+    </div>
+</section>
