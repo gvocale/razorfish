30f421e2dd68cd2588ee36eef707f353975df7ce
Giovanni <gvocale@gmail.com>
2016-05-05 16:20:18 -0400

    added image map on 404 page

--- razorfish-prototypes/dist/js/jquery.gio.js
+++ razorfish-prototypes/dist/js/jquery.gio.js
@@ -583,9 +583,17 @@ $(function() {
     //     });
     // }
 
+    // apply gradient map
 
 
-  
+
+
+    // Code used to generate Gradient Map
+    
+    // var target = document.getElementById('myimage');
+    // var gradientMap = "#242926, #42585c";
+
+    // GradientMaps.applyGradientMap(target, gradientMap);
 
 
 
