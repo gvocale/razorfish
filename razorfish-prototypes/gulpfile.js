f65f72ad944f4c243f81df7b8b00b277a1ad9108
Giovanni <gvocale@gmail.com>
2016-07-05 13:36:31 -0400

    fixed qa links

--- razorfish-prototypes/gulpfile.js
+++ razorfish-prototypes/gulpfile.js
@@ -1,23 +1,20 @@
+// Include gulp
 var gulp = require('gulp');
+
+// Include Our Plugins
 var browserSync = require('browser-sync').create();
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
+var iconfont = require('gulp-iconfont');
+
+var runTimestamp = Math.round(Date.now() / 1000);
 
 var sassOptions = {
     errLogToConsole: true,
     outputStyle: 'expanded'
 };
 
-// var consolidate = require('gulp-consolidate');
-// var rename = require('gulp-rename');
-// var iconfont = require('gulp-iconfont');
-
-// var fontName = 'iconfont'; // name of font
-
-
-var iconfont = require('gulp-iconfont');
-var runTimestamp = Math.round(Date.now() / 1000);
-
+// IconFont Task
 gulp.task('iconfont', function() {
     return gulp.src(['src/icons/*.svg'])
         .pipe(iconfont({
@@ -34,38 +31,6 @@ gulp.task('iconfont', function() {
         .pipe(gulp.dest('dist/fonts/'));
 });
 
-// gulp.task('generateIconfont', function(){
-//   gulp.src(['src/icons/*.svg']) // the location of all the svg files to be created into the font
-//     .pipe(iconfont({
-//       normalize: true,
-//       fontName: fontName,
-//       appendCodepoints: true
-//     }))
-//     // automatically assign a unicode value to the icon
-//     .on('codepoints', function(codepoints, options) {
-//       codepoints.forEach(function(glyph, idx, arr) {
-//         arr[idx].codepoint = glyph.codepoint.toString(16)
-//       });
-//       // -------------------------------
-//       // START additional stuff to generate an scss file with all the font characters inside it
-//       // -------------------------------
-//       gulp.src('src/sass/_modules/_font-template.scss') // a template scss file, used to generate the scss code for all the icons
-//       .pipe(consolidate('lodash', {
-//         // see the font-template.scss file to see how the following 'fontName' and 'fontPath' values are used
-//         glyphs: codepoints,
-//         fontName: options.fontName,
-//         fontPath: 'src/fonts/',
-//         className: 'icon'
-//       }))
-//       .pipe(rename('_iconfont.scss')) // rename the scss file (otherwise it takes the same file name as "font-template.scss")
-//       .pipe(gulp.dest('src/sass/_modules/')); // where to save "_iconfont.scss"
-//       // -------------------------------
-//       // END additional stuff to generate an scss file with all the font characters inside it
-//       // -------------------------------
-//     })
-//     .pipe(gulp.dest('dist/fonts')); // where to save the generated font files
-// });
-
 
 // Compile sass into CSS & auto-inject into browsers
 gulp.task('sass', function() {
@@ -80,7 +45,6 @@ gulp.task('sass', function() {
 
 // Static Server + watching scss/html files
 gulp.task('serve', ['sass'], function() {
-
     browserSync.init({
         server: {
             baseDir: "dist",
@@ -88,7 +52,6 @@ gulp.task('serve', ['sass'], function() {
         },
         browser: "google chrome"
     });
-
     gulp.watch("src/sass/**/*.scss", ['sass']);
     gulp.watch("src/icons/*.svg", ['iconfont']);
     gulp.watch("dist/*.html").on('change', browserSync.reload);
@@ -110,7 +73,6 @@ gulp.task('icimsSass', function() {
 
 // Static Server + watching scss/html files
 gulp.task('icimsTask', ['icimsSass'], function() {
-
     gulp.watch("src/sass/_modules/icims.scss", ['sass']);
 });
 
